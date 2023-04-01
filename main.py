f4610fc70b81269a80b3d42896b593c27089b684
Bimali-Tissera <bimali.20210337@iiit.ac.lk>
2023-04-01 21:03:06 +0530

    Implemented classifier

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from joblib import load
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Msg(BaseModel):
-    msg: str
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
+class Query(BaseModel):
+    uid: str
+    query: str
 
 
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
+def classifyQuery(query):
+    model = load('./model/model.joblib')
+    fitted_vectorizer = load('./model/fitted_vectorizer.joblib')
+    typeId, = model.predict(fitted_vectorizer.transform([query]))
+
+    match typeId:
+        case 0:
+            return "Task"
+        case 1:
+            return "Event"
+        case 2:
+            return "Routine"
+        case 3:
+            return "Project"
+        case 4:
+            return "Venture"
 
 
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
+def extractEntities(query):
+    pass
 
 
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/query")
+async def createQuery(query: Query):
+    extracted=extractEntities(query.query)
+    return {"status": "success", "data": {"uid": query.uid, "name": "", "type": classifyQuery(query.query), "mode": "", "s": "", "duration": "", "projectId": "", "reminder": "", "status": ""}}
